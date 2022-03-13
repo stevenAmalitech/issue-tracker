9eae3a77c0e0f9deade323a722273967e0b53624
Steven Yirenkyi <yirenkyisteven7@gmail.com>
2022-03-13 15:09:29 +0000

    serve client

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express from "express";
+import { join } from "path";
 
 const app = express();
 
 app.use(express.json());
 
+app.use(express.static(join(__dirname, "../client/dist")));
+
 app.get("/", (req, res) => {
   res.send("ok");
+});
+
+app.get("/*", (req, res) => {
+  res.sendFile(join(__dirname, "../client/dist/index.html"));
 });
 
 export default app;
