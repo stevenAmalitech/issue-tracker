8732244fcd5c45facc1cf0ce8003c48d2445dee4
Steven Yirenkyi <yirenkyisteven7@gmail.com>
2022-03-18 20:08:17 +0000

    navigation bar

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,77 @@
+import { Link } from "react-router-dom";
+
+export default function NavBar(props) {
+  function showNav() {
+    document.getElementById("mobile-menu").classList.toggle("hidden");
+  }
+
+  const menuItems = [
+    { name: "issues" },
+    { name: "projects" },
+    { name: "clients" },
+  ];
+
+  return (
+    <nav className="shadow-md px-2 sm:px-6 py-4">
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
+        <div></div>
+        <button
+          data-collapse-toggle="mobile-menu"
+          type="button"
+          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 md:hidden rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+          aria-controls="mobile-menu-2"
+          aria-expanded="false"
+          onClick={showNav}
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-6 h-6"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+          <svg
+            className="hidden w-6 h-6"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </button>
+
+        <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
+          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+            {menuItems.map(({ name }) => (
+              <MenuItem key={name} name={name} />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+const MenuItem = ({ name, link }) => {
+  console.log({ name, link });
+  return (
+    <li>
+      <Link
+        to={name}
+        className="block capitalize py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+      >
+        {name}
+      </Link>
+    </li>
+  );
+};
